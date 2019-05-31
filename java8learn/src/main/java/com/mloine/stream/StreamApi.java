3ce2964787bfb1e7ebe933690ff04b65f51fcf01
companyXYK <m15618975413@163.com>
2019-05-31 17:59:40 +0800

    1.添加数组引用

---
+++ java8learn/src/main/java/com/mloine/stream/StreamApi.java
@@ -0,0 +1,13 @@
+package com.mloine.stream;
+
+/**
+ * @className: StreamApi
+ * @Description:TOO
+ *              1.创建stream
+ *              2.中间操作
+ *              3.终止操作（终端操作）
+ * @Author:mloine
+ * @Date：2019/5/3112:44
+ **/
+public class StreamApi {
+}
